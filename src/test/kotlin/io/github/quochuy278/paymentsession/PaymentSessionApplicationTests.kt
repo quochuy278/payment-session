ad9eef3a7f13675b1300869bd94bf59519421dae
quochuy278 <quochuy270800@gmail.com>
2026-07-29 12:48:39 +0200

    Convert Spring Boot bootstrap to Kotlin

---
+++ src/test/kotlin/io/github/quochuy278/paymentsession/PaymentSessionApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.quochuy278.paymentsession
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentSessionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
