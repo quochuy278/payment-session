76849d7ebd267b5b2a4cddf5a81f8e95d27f1130
quochuy278 <quochuy270800@gmail.com>
2026-07-29 12:48:39 +0200

    Convert Spring Boot bootstrap to Kotlin

---
+++ src/main/kotlin/io/github/quochuy278/paymentsession/PaymentSessionApplication.kt
@@ -0,0 +1,11 @@
+package io.github.quochuy278.paymentsession
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentSessionApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaymentSessionApplication>(*args)
+}
