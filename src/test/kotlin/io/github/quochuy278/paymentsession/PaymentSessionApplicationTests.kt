3437a7effbfb16a7a7713c3530e3d1d10a5e2f0b
quochuy278 <quochuy270800@gmail.com>
2026-08-02 21:14:03 +0200

    Configure application environments

--- src/test/kotlin/io/github/quochuy278/paymentsession/PaymentSessionApplicationTests.kt
+++ src/test/kotlin/io/github/quochuy278/paymentsession/PaymentSessionApplicationTests.kt
@@ -2,8 +2,10 @@ package io.github.quochuy278.paymentsession
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaymentSessionApplicationTests {
 
 	@Test
